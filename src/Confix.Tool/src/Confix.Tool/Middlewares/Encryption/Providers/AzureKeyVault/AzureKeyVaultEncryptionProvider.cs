97224e59445b3c4ffab01ebc01013dc879a46379
Mariusz Matysek <mariusz.matysek@swisslife.ch>
2023-07-06 15:27:16 +0200

    Decrypt with chunks

--- src/Confix.Tool/src/Confix.Tool/Middlewares/Encryption/Providers/AzureKeyVault/AzureKeyVaultEncryptionProvider.cs
+++ src/Confix.Tool/src/Confix.Tool/Middlewares/Encryption/Providers/AzureKeyVault/AzureKeyVaultEncryptionProvider.cs
@@ -8,20 +8,25 @@ namespace Confix.Tool.Middlewares.Encryption.Providers.AzureKeyvault;
 
 public sealed class AzureKeyVaultEncryptionProvider : IEncryptionProvider
 {
+    private const int ChunkSize = 250;
     private readonly CryptographyClient _client;
 
     public AzureKeyVaultEncryptionProvider(JsonNode configuration)
         : this(AzureKeyVaultEncryptionProviderConfiguration.Parse(configuration))
-    { }
+    {
+    }
 
-    public AzureKeyVaultEncryptionProvider(AzureKeyVaultEncryptionProviderConfiguration configuration)
+    public AzureKeyVaultEncryptionProvider(
+        AzureKeyVaultEncryptionProviderConfiguration configuration)
         : this(AzureKeyVaultEncryptionProviderDefinition.From(configuration))
-    { }
+    {
+    }
 
     public AzureKeyVaultEncryptionProvider(AzureKeyVaultEncryptionProviderDefinition definition)
         : this(new KeyClient(new Uri(definition.Uri), new DefaultAzureCredential())
             .GetCryptographyClient(definition.KeyName, definition.KeyVersion))
-    { }
+    {
+    }
 
     public AzureKeyVaultEncryptionProvider(CryptographyClient client)
     {
@@ -31,22 +36,54 @@ public sealed class AzureKeyVaultEncryptionProvider : IEncryptionProvider
     public const string Type = "AzureKeyVault";
 
     public Task<byte[]> DecryptAsync(byte[] data, CancellationToken cancellationToken)
-    => KeyVaultExtension.HandleKeyVaultException(async () =>
-    {
-        DecryptResult decrypted = await _client.DecryptAsync(
-            EncryptionAlgorithm.RsaOaep256,
-            data,
-            cancellationToken);
-        return decrypted.Plaintext;
-    });
+        => KeyVaultExtension.HandleKeyVaultException(async () =>
+        {
+            using var inputStream = new MemoryStream(data);
+            using var outputStream = new MemoryStream();
+
+            var totalBytes = inputStream.Length;
+            var buffer = new byte[512];
+            int bytesRead;
+            long totalBytesRead = 0;
+
+            while ((bytesRead =
+                await inputStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
+            {
+                DecryptResult decrypted = await _client.DecryptAsync(EncryptionAlgorithm.RsaOaep256,
+                    buffer, cancellationToken);
+                await outputStream.WriteAsync(decrypted.Plaintext, 0, decrypted.Plaintext.Length,
+                    cancellationToken);
+
+                totalBytesRead += bytesRead;
+                Console.WriteLine($"Encrypting... {totalBytesRead}/{totalBytes} bytes");
+            }
+
+            return outputStream.ToArray();
+        });
 
     public Task<byte[]> EncryptAsync(byte[] data, CancellationToken cancellationToken)
-    => KeyVaultExtension.HandleKeyVaultException(async () =>
-    {
-        EncryptResult encrypted = await _client.EncryptAsync(
-            EncryptionAlgorithm.RsaOaep256,
-            data,
-            cancellationToken);
-        return encrypted.Ciphertext;
-    });
+        => KeyVaultExtension.HandleKeyVaultException(async () =>
+        {
+            using var inputStream = new MemoryStream(data);
+            using var outputStream = new MemoryStream();
+
+            var totalBytes = inputStream.Length;
+            var buffer = new byte[ChunkSize];
+            int bytesRead;
+            long totalBytesRead = 0;
+
+            while ((bytesRead =
+                await inputStream.ReadAsync(buffer, 0, buffer.Length, cancellationToken)) > 0)
+            {
+                EncryptResult encrypted = await _client.EncryptAsync(EncryptionAlgorithm.RsaOaep256,
+                    buffer, cancellationToken);
+                await outputStream.WriteAsync(encrypted.Ciphertext, 0, encrypted.Ciphertext.Length,
+                    cancellationToken);
+
+                totalBytesRead += bytesRead;
+                Console.WriteLine($"Encrypting... {totalBytesRead}/{totalBytes} bytes");
+            }
+
+            return outputStream.ToArray();
+        });
 }
